b5c35f46fed9a9c5e1c70154ca8e3a42826fa7df
MonkeyBoy <monkeyboy@MonkeyBoydeMacBook-Pro.local>
2025-05-22 11:57:09 +0800

    更新项目文件，准备部署

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const app = document.getElementById('app');
-    if (app) {
-        app.textContent = '学城搜索';
-    }
+    if (!app) return;
+
+    const text = '学城搜索';
+    const textElement = document.createElement('div');
+    textElement.className = 'text-display';
+    textElement.textContent = text;
+    
+    app.appendChild(textElement);
 }); 
